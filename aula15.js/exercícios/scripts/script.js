3d2424b5b127a4f5dccf13345d9fca16fb88e999
ANDRE658 <126590811+ANDRE658@users.noreply.github.com>
2023-07-20 20:43:14 -0300

    Update script.js

--- aula15.js/exercícios/scripts/script.js
+++ aula15.js/exercícios/scripts/script.js
@@ -3,10 +3,10 @@ const numeroTitle = document.querySelector ("#numeroTitle");
 const texto = document.getElementById('texto');
 
 numeroTitle.innerHTML = numeroPrompt;
-texto.innerHTML = `<p>Raiz quadrada: ${numeroPrompt ** 0.5}</p>
-<p>${numeroPrompt} é inteiro? = ${Number.isInteger(numeroPrompt)}</p>
-<p>É NaN: ${Number.isNaN(numeroPrompt)}</p>
-<p>Arredondando o número para baixo ${Math.floor(numeroPrompt)}</p>
-<p>Arredondando o número para cima ${Math.ceil(numeroPrompt)}</p>
-<p>Com duas casas decimas: ${numeroPrompt.toFixed(2)}</p>`;
+texto.innerHTML += `<p>Raiz quadrada: ${numeroPrompt ** 0.5}</p>`
+texto.innerHTML += `<p>É NaN: ${Number.isNaN(numeroPrompt)}</p>`
+texto.innerHTML += `<p>${numeroPrompt} é inteiro? = ${Number.isInteger(numeroPrompt)}</p>`
+texto.innerHTML += `<p>Arredondando o número para baixo ${Math.floor(numeroPrompt)}</p>`
+texto.innerHTML += `<p>Arredondando o número para cima ${Math.ceil(numeroPrompt)}</p>`
+texto.innerHTML += `<p>Com duas casas decimas: ${numeroPrompt.toFixed(2)}</p>`;
 
