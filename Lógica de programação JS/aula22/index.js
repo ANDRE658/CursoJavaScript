69b68a9f700a0efbde2ee3cc38bf653dcb2f2e48
ANDRE658 <126590811+ANDRE658@users.noreply.github.com>
2023-07-24 19:13:43 -0300

    operadores lógico

---
+++ Lógica de programação JS/aula22/index.js
@@ -0,0 +1,10 @@
+/*
+Operadores Lógico
+&& and  e 
+|| or  ou
+! not  não
+*/ 
+const expreçãoAnd = true && true && true;
+const expreçãoOr = true || false || false;
+const expreçãoNot = (!true);
+console.log(expreçãoAnd, expreçãoOr, expreçãoNot)
