3dfad5c918fe607fcc687f6983f6575acd168a1d
ANDRE658 <126590811+ANDRE658@users.noreply.github.com>
2023-07-24 18:13:21 -0300

    aula de operadores aritiméticos

---
+++ Lógica de programação JS/aula21/index.js
@@ -0,0 +1,17 @@
+/*operadores de comparação
+> maior que
+>= maior ou igual
+< menor que
+<= menor ou igual
+== iguldade
+=== igualdade estrita(checa valor e tipo)
+!= diferente
+!== diferente estrito(checa valor e tipo)
+*/
+console.log(10>5); // retorna boolean
+console.log(10==10); // retorna boolean
+console.log(10=='10'); // retorna boolean - faz a coerção de dados (não recomendado)
+console.log(10!='10'); // retorna boolean - faz a coerção de dados (não recomendado)
+console.log(10==='10'); // retorna boolean - faz acomparação de tipos tambem
+console.log(10!=='10'); // retorna boolean - faz acomparação de tipos tambem
+
