7b8aaedbf6dd54369f439b74462dd54d06b7e466
ANDRE658 <126590811+ANDRE658@users.noreply.github.com>
2023-07-24 19:21:46 -0300

    Update index.js

--- Lógica de programação JS/aula22/index.js
+++ Lógica de programação JS/aula22/index.js
@@ -8,3 +8,7 @@ const expreçãoAnd = true && true && true;
 const expreçãoOr = true || false || false;
 const expreçãoNot = (!true);
 console.log(expreçãoAnd, expreçãoOr, expreçãoNot)
+
+//avalhação de curto circuito -> retorna o valor mesmo sendo ela falsa
+// vai retornar  valor verdadeiro (se em 10 dados o primeiro verdadeiro será retornado pelo or( para o and é o mesmo qundo o valor é false))
+
