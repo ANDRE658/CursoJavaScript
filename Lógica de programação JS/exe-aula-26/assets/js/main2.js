1115a3d51ef1a5e2a47d3fe99f0f35b9d8a082b9
ANDRE658 <126590811+ANDRE658@users.noreply.github.com>
2023-09-10 09:51:09 -0300

    Mexi e deu merda, tem que arrumar.

---
+++ Lógica de programação JS/exe-aula-26/assets/js/main2.js
@@ -0,0 +1,65 @@
+const form = document.querySelector(".form");
+const inputPeso = document.querySelector("#inputPeso");
+const inputAltura = document.querySelector("#inputAltura");
+
+
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    const inputPeso = e.target.querySelector("#inputPeso");
+    const inputAltura = e.target.querySelector("#inputAltura");
+
+    const peso = Number(inputPeso.value);
+    const altura = Number(inputAltura.value);
+
+    if (!peso){
+        setResultado('Peso inválido', false);
+        return;
+    }
+    if (!altura){
+        setResultado('Altura inválido', false);
+        return;
+    }
+    
+    const imc = getImc(peso, altura);
+    const nivelImc = getnivelImc(imc);
+    const msg = `Seu IMC é ${imc} (${nivelImc})`;
+    console.log(nivelImc)
+});
+
+function getnivelImc (imc){
+    const nivel = ['Abaixo do peso', 'Peso normal', 'Sobrepeso', 'Obesidade grau 1',
+     'Obesidade grau 2', 'Obesidade grau 3'];
+
+    if (imc >= 39.9) return nivel[5];
+    if (imc >= 34.9) return nivel[4];
+    if (imc >= 29.9) return nivel[3];
+    if (imc >= 24.9) return nivel[2];
+    if(imc >=18.5) return nivel[1];
+    if(imc < 18.5) return nivel[0];
+    
+}
+
+function getImc (peso, altura){
+    const imc = peso / altura**2;
+    return imc.toFixed(2);
+}
+
+function criaP (){
+    const p = document.createElement ('p');
+    return p;
+}
+
+function setResultado ( msg, isValid ){
+    const resposta = document.querySelector("#resposta");
+    resposta.innerHTML = '';
+
+    
+    const p = criaP();
+    if (isValid){
+        p.classList.add('paragrafo-resposta');
+    }else {
+        p.classList.add('bad');
+    }
+    p.innerHTML = msg;
+    resposta.appendChild(p);
+}
