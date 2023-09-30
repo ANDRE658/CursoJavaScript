0e47257c79ed8a8784c89e0c127ed112e05846ad
ANDRE658 <126590811+ANDRE658@users.noreply.github.com>
2023-09-30 16:53:00 -0300

    é isso

---
+++ Lógica de programação JS/aula29/index.js
@@ -0,0 +1,12 @@
+const data = new Date(); 
+console.log("Dia "+data.getDate());
+console.log("Mês "+data.getMonth());
+console.log("Ano "+data.getFullYear());
+console.log("Hora "+data.getHours());
+console.log("Min "+data.getMinutes());
+console.log("Sec "+data.getSeconds());
+console.log("ms "+data.getMilliseconds());
+console.log("Dia da semana "+data.getDay());
+console.log(Date.now())
+
+
---
+++ Lógica de programação JS/aula30/index.js
@@ -0,0 +1,3 @@
+const data = new Date('1987-04-21 00:00:00');
+const dia_semana = data.getDay();
+let dia_semana_Texto;
