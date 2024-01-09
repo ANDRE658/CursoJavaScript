88714305fcd04d6792d90ef850449705fb3312ec
ANDRE658 <126590811+ANDRE658@users.noreply.github.com>
2024-01-09 20:12:57 -0300

    Escolha caso

---
+++ Lógica de programação JS/aula28/index.js
@@ -0,0 +1,29 @@
+// Ecolha caso - Switch case
+const data = new Date();
+const diaSemana = data.getDay();
+let diaSemanaTexto
+
+switch (diaSemana){
+case 0:
+    diaSemanaTexto = 'Domingo';
+    break;
+case 1:
+    diaSemanaTexto = 'Segunda-feira';
+    break;
+case 2:
+    diaSemanaTexto = 'Terça-feira';
+    break;
+case 3:
+    diaSemanaTexto = 'Quarta-feira';
+    break;
+case 4:
+    diaSemanaTexto = 'Quinta-feira';
+    break;
+case 5:
+    diaSemanaTexto = 'Sexta-feira';
+    break;
+case 6:
+    diaSemanaTexto = 'Sabado';
+    break;
+}
+console.log(diaSemanaTexto)
